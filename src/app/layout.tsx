688099005fe45dc8c5aa58eb570aa97c3f78a371
João Víctor Arantes <arantesjoaov@gmail.com>
2025-10-11 18:27:58 -0300

    feat: more tests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,6 @@ const openSans = Open_Sans({
 export const viewport: Viewport = {
   themeColor: "#fcfcf5",
   width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  interactiveWidget: "resizes-content",
 };
 
 export const metadata: Metadata = {
